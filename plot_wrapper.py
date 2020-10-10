de380de446c3bd0825e9964bf855b37e8f9ee09f
Cody <handfulofsharks@gmail.com>
2020-10-10 12:59:45 -0700

    Adds plotting to simulation

--- plot_wrapper.py
+++ plot_wrapper.py
@@ -1,6 +1,24 @@
 import seaborn as sns
-import pandas as pd
-
+from matplotlib import pyplot as plt
 
 def plot_wrapper(df):
-    import pdb; pdb.set_trace()
+    for metric in get_metrics(df):
+        plot_metrics(df, metric)
+    
+    
+def plot_metrics(df, metric):
+    ax = sns.lineplot(data=df,x="frame_rate",y=metric, hue="scenario")
+    plt.grid(None)
+    plt.show()
+    
+    
+def get_scenarios(df):
+    return list(set(df.scenario.values.tolist()))
+
+
+def get_metrics(df):
+    return df.columns.values.tolist()[2:]
+
+
+def get_x_ticks(df):
+    return list(set(df.frame_rate.values.tolist()))
