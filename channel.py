7186368bdf6e477c7fc66fdcf802996291f239da
Finn Willard <finn@Finns-MacBook-Pro.local>
2020-10-11 13:47:45 -0700

    VCS for scenario 1

--- channel.py
+++ channel.py
@@ -1,4 +1,4 @@
 class Channel:
     def __init__(self, sim_params):
         self.is_idle = True
-        self.idle_count = sim_params.frame_size_slots + sim_params.SIFS_dur + sim_params.ACK_dur
+        self.idle_count = sim_params.frame_size_slots + sim_params.SIFS_dur + sim_params.ACK_dur + 2 + 2 +  1 + 1 #RTS + CTS + 2x SIFS
--- main.py
+++ main.py
@@ -129,7 +129,102 @@ def Scenario1_CSMA(sim_params, frame_rate):
     return [frame_rate, collisions, a_succ, c_succ, a_thruput, c_thruput]
 
 def Scenario1_VCS(sim_params, frame_rate):
-    return
+    A = Node(sim_params, frame_rate, seed=np.random.randint(0,100))
+    C = Node(sim_params, frame_rate, seed=np.random.randint(0,100))
+    channel = Channel(sim_params)
+    collisions = 0
+    a_succ = 0
+    c_succ = 0
+
+    #number of slots for 10 seconds
+    max_slots = math.ceil(sim_params.max_sim_time_sec/sim_params.slot_dur_us)
+    
+    for slot in range(0, max_slots):
+        #checks if a packet is ready for transmit and adds it to queue
+        A.check_packet_ready(slot)
+        C.check_packet_ready(slot)
+        if not channel.is_idle:
+            channel.idle_count -= 1
+            if channel.idle_count <= 0:
+                channel.is_idle = True
+                channel.idle_count = sim_params.frame_size_slots + sim_params.SIFS_dur + sim_params.ACK_dur + 2 + 2 +  1 + 1
+                if A.state == State.transmitting:
+                    A.state = State.idle
+                if C.state == State.transmitting:
+                    C.state = State.idle
+            continue
+        if A.state == State.ready_to_transmit:
+            if A.backoff is None:
+                A.calc_backoff()
+            A.state = State.waiting_to_transmit
+    
+        if C.state == State.ready_to_transmit:
+            if C.backoff is None:
+                C.calc_backoff()
+            C.state = State.waiting_to_transmit
+        
+        if A.state == State.waiting_to_transmit:
+            #wait until channel is idle for DIFS slots
+            if channel.is_idle:
+                A.difs_duration -= 1
+            else:
+                A.difs_duration = 2
+            if A.difs_duration <= 0:
+                #decrement backoff slots
+                if channel.is_idle:
+                    A.backoff -= 1
+                    if A.backoff <= 0:
+                        #send RTS + NAV
+                        A.state = State.transmitting
+        
+        if C.state == State.waiting_to_transmit:
+            if channel.is_idle:
+                C.difs_duration -= 1
+            else:
+                C.difs_duration = 2
+            if C.difs_duration <= 0:
+                #decrement backoff slots
+                if channel.is_idle:
+                    C.backoff -= 1
+                    if C.backoff <= 0:
+                        #send RTS + NAV
+                        C.state = State.transmitting
+        
+        if A.state == State.transmitting and C.state == State.transmitting:
+            #collision
+            collisions += 1
+            A.cw = A.cw * 2
+            A.backoff = None
+            A.state = State.idle
+            A.difs_duration = 2
+            C.cw = C.cw * 2
+            C.backoff = None
+            C.state = State.idle
+            C.difs_duration = 2
+        elif A.state == State.transmitting and not C.state == State.transmitting:
+            channel.is_idle = False
+            A.backoff = None
+            a_succ += 1
+            A.cw = A.cw_0
+            C.cw = C.cw_0
+            A.queue.get()
+            A.difs_duration = 2
+            C.difs_duration = 2
+
+        elif not A.state == State.transmitting and C.state == State.transmitting:
+            channel.is_idle = False
+            C.backoff = None
+            c_succ += 1
+            A.cw = A.cw_0
+            C.cw = C.cw_0
+            C.queue.get()
+            A.difs_duration = 2
+            C.difs_duration = 2
+
+    a_thruput = 8*(a_succ * sim_params.frame_size_bytes/sim_params.max_sim_time_sec)/10e3
+    c_thruput = 8*(c_succ * sim_params.frame_size_bytes/sim_params.max_sim_time_sec)/10e3
+    return [frame_rate, collisions, a_succ, c_succ, a_thruput, c_thruput]
+                        
 
 def Scenario2_CSMA(sim_params, frame_rate):
     A = Node(sim_params, frame_rate, seed=np.random.randint(0,100))
